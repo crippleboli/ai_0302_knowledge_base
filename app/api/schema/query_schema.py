7e6e2e53fe156bdba1f0a7fdb7bbc452fca0ff94
crippleboli <113300829+crippleboli@users.noreply.github.com>
2026-06-11 21:15:42 +0800

    feat: 四个接口和事件类型

--- app/api/schema/query_schema.py
+++ app/api/schema/query_schema.py
@@ -1,16 +1,17 @@
 from pydantic import BaseModel
 
+class QueryRequestParam(BaseModel):
+    query:str
+    session_id:str
+    is_stream:bool=False
 
-# 上传文件的响应数据类型
-class UploadSchema(BaseModel):
-    code:int = 200
+class QueryStreamResponse(BaseModel):
     message:str
-    task_ids:list[str]
+    session_id:str
 
-# 查询任务状态的数据类型
-class TaskStatusSchema(BaseModel):
-    code:int = 200
-    task_id:str  # 当前查询的task_id
-    status:str  #  # 当前task_id 对应的整体和宏观状态 processing 处理中 completed 已完成   failed 失败
-    done_list:list[str]  # 当前task_id已完成的任务列表 可能有多个
-    running_list:list[str] # 当前task_id正在运行的任务列表 同一时刻只有一个
+class QueryNotStreamResponse(BaseModel):
+    message: str
+    session_id: str
+    answer:str
+    done_list:list
+    image_urls:list
